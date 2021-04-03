573b4689efe131ee6bb7d3f7b1d774b69b272a9e
Samuel Roth <samuel6roth@gmail.com>
2021-04-02 20:08:12 -0500

    Got JS to generate Readme

--- index.js
+++ index.js
@@ -7,34 +7,44 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        message: 'What was your motitvation?',
-        name: 'motivation'
+        message: 'What is the title of your project?',
+        name: 'title'
     },
-    {
-        type: 'input',
-        message: 'Why did you build this project?',
-        name: 'project'
-    },
-    {
-        type: 'input',
-        message: 'What problem does this project solve?',
-        name: 'problem'
-    },
-    {
-        type: 'input',
-        message: 'What did you learn when creating this project?',
-        name: 'learn'
-    },
-    {
-        type: 'input',
-        message: 'What makes your project stand out?',
-        name: 'standout'
-    },
-    {
-        type: 'input',
-        message: 'What challenges did you face when creating this project?',
-        name: 'challenges'
-    },
+    // {
+    //     type: 'input',
+    //     message: 'Please describe your project.',
+    //     name: 'description'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What was your motitvation?',
+    //     name: 'motivation'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Why did you build this project?',
+    //     name: 'project'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What problem does this project solve?',
+    //     name: 'problem'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What did you learn when creating this project?',
+    //     name: 'learn'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What makes your project stand out?',
+    //     name: 'standout'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What challenges did you face when creating this project?',
+    //     name: 'challenges'
+    // },
     {
         type: 'input',
         message: 'What features do you plan to implement in the future?',
@@ -43,11 +53,31 @@ const questions = [
     
 ]
 
+
+const generator = ({ title, future }) => {
+return `${title}
+${future}
+`
+}
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+inquirer.prompt(questions)
+    .then((response) => {
+    const readMe = generator(response)
+    fs.writeFile('README.md', readMe, (err) =>
+    err ? console.error(err) : console.log('Prompts written!'))
+});
+
+
+
+
+
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
